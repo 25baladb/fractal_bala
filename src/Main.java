1479b39617ca557d5d4744caa7afb3601e464e1a
25baladb <>
2023-04-05 23:08:27 -0700

    completed

---
+++ src/Main.java
@@ -0,0 +1,53 @@
+import processing.core.PApplet;
+import java.util.Random;
+public class Main extends PApplet {
+    public static PApplet app;
+    int d = 800;
+    public Main(){
+        super();
+        app = this;
+    }
+    public static void main (String[] args) {
+        PApplet.main("Main");
+    }
+
+    public void settings(){
+        size(d, d);
+    }
+
+    public void setup(){}
+
+    public void draw(){
+        dS(d/2, d/2, d);
+        someShape(d, d, 10);
+    }
+
+    private void dS(int x, int y, int w){
+        Main.app.noStroke();
+        Random r = new Random();
+        float red = r.nextInt(255);
+        float green = r.nextInt(255);
+        float blue = r.nextInt(255);
+        Main.app.fill(red, green, blue);
+        rect(x, y, w, w);
+        if(w > 10){
+            dS(x, y + w/2, w/2);
+            dS(x, y - w/2, w/2);
+            dS(x + w/2, y, w/2);
+            dS(x - w/2, y, w/2);
+        }
+    }
+    private void someShape(int x, int y, int H){
+        Main.app.noStroke();
+        Random r = new Random();
+        float red = r.nextInt(255);
+        float green = r.nextInt(255);
+        float blue = r.nextInt(255);
+        Main.app.fill(red, green, blue);
+        ellipse(x-H, y-H, H*2, H*2);
+        if(H >= 2){
+            H--;
+            someShape(d, d, H);
+        }
+    }
+}
